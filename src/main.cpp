3d9b2fa9a5ed4a5676bf2c366539b9834d559e42
TheBotlyNoob <jj@jjay.dev>
2025-03-05 15:52:12 -0500

    fix ubuntu build

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,15 @@
 #include "imgui_impl_dx11.h"
 #include "imgui_impl_win32.h"
 #include <d3d11.h>
-#include <hidsdi.h>
-#include <hidusage.h>
 #include <iostream>
 #include <stdint.h>
 #include <tchar.h>
 #include <windows.h>
+
+extern "C" {
+#include <hidsdi.h>
+#include <hidusage.h>
+}
 
 // the goal FPS for both the GUI and the message handlers (will impact
 // keypresses)
